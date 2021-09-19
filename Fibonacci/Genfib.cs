e4316b1bc2108260c4525b175defe50cf6d8c201
KevinLang26 <90727834+KevinLang26@users.noreply.github.com>
2021-09-19 16:01:55 -0700

    Add files via upload

---
+++ Fibonacci/Genfib.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fibonacci
+{
+    public class Genfib
+    {
+        public int Generate(int num)
+        {
+            int[] fibSeq = new int[num + 1];
+            if (num == 0)
+            {
+                return 0;
+            }
+            else if (num == 1)
+            {
+                return 1;
+            }
+            fibSeq[0] = 0;
+            fibSeq[1] = 1;
+            for (int i = 2; i <= num; i++)
+            {
+                fibSeq[i] = fibSeq[i - 1] + fibSeq[i - 2];
+            }
+            return fibSeq[num];
+        }
+    }
+}
---
+++ Fibonacci/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Fibonacci;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Genfib fib = new Genfib();
+            int n = 10;
+            var result = fib.Generate(n);
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
